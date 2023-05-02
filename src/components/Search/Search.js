200390cdfa4dd4223cda50c271de06fb3e2ec51b
abenetwolde <tecnoabi4@gmail.com>
2023-05-02 12:24:45 +0300

    sjd

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,7 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import "./Search.css"
 const Searchbar = () => {
 
     const [keyword, setKeyword] = useState("");
@@ -19,7 +19,7 @@ const Searchbar = () => {
     return (
         <form onSubmit={handleSubmit} className="w-90w my-5 mx-5 sm:w-9/12  px-1 sm:px-4 py-1.5 flex justify-between items-center shadow-md  rounded-sm overflow-hidden">
             <input value={keyword} onChange={(e) => setKeyword(e.target.value)} className="text-sm flex-1 outline-none border-none " type="text" placeholder="Search for products, brands and more" />
-            <button type="submit" className=""><SearchIcon /></button>
+            <button type="submit" className="text-primary-black"><SearchIcon /></button>
         </form>
     );
 };
